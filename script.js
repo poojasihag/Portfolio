82f5a41dfc4a4dbfcc211275ab0990f718cede60
Suraj Kumar <ksuraj3290@gmail.com>
2024-05-11 01:46:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Hello, I am <span class='colorName'>Pooja</span> a Web Developer.",
+    "Welcome to my portfolio.",
+  ],
+  typeSpeed: 70,
+  backSpeed: 30,
+  loop: true,
+  smartBackspace: true,
+  cursorChar: "🖱",
+});
